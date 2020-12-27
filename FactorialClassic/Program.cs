e8395f40fb649131c5f962bb82f3b721392be73e
WIN-O6MLADSHQ11\User <vimmelbuch@gmail.com>
2020-12-27 17:55:22 +0200

    FactorialClassic

---
+++ FactorialClassic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FactorialClassic
+{
+    class Program
+    {
+        static int Factorial(int factorial)
+        { 
+            if (factorial > 1)
+            {
+                int result = Factorial(factorial-1);
+                return factorial * result;
+            }
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            int factorial = Int32.Parse(Console.ReadLine());
+            int factor = Factorial(factorial);
+            Console.WriteLine(factor);
+        }
+    }
+}
